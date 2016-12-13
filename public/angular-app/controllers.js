e28cdbd50424754cf4cf297e6a3bb6e6abcd5736
Chao Liu <liuchao8989@gmail.com>
2016-12-12 23:24:22 -0800

    add nav bar

--- public/angular-app/controllers.js
+++ public/angular-app/controllers.js
@@ -3,3 +3,12 @@ storyMap.controller('MainController', ['$scope', function($scope){
   var self = this;
   $scope.title= "Main"
 }])
+
+
+storyMap.controller('LoginController',LoginController);
+
+function LoginController(){
+  var self = this;
+  self.title = "LoginController";
+
+}
---
+++ public/angular-app/directives.js
@@ -0,0 +1,11 @@
+console.log("div controller");
+
+storyMap.directive('navBar', navBar);
+function navBar(){
+  return {
+    restrict: 'E',
+    templateUrl: 'angular-app/directives/navBar.html',
+    controller: 'LoginController',
+    controllerAs: 'vm'
+  };
+};
